34a535d62457f4f27bd0dbe337be65f8858a3007
Osama Al-mamari <yemencoder@gmail.com>
2023-01-27 20:17:11 +0300

    chating

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,84 +1,114 @@
 import 'package:flutter/material.dart';
 import 'package:chat_firebase/constants.dart';
+import 'package:chat_firebase/models/message.dart';
+import 'package:chat_firebase/widgets/chat_buble.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+
 class ChatPage extends StatelessWidget {
   static String id = 'ChatPage';
-  CollectionReference messages =
-  FirebaseFirestore.instance.collection(kMessagesCollections);
 
+  final _controller = ScrollController();
+
+  CollectionReference messages =
+      FirebaseFirestore.instance.collection(kMessagesCollections);
+  TextEditingController controller = TextEditingController();
 
 
 
   @override
   Widget build(BuildContext context) {
- var email  = ModalRoute.of(context)!.settings.arguments ;
- return Scaffold(
-   appBar: AppBar(
-     automaticallyImplyLeading: false,
-     backgroundColor: kPrimaryColor,
-     title: Row(
-       mainAxisAlignment: MainAxisAlignment.center,
-       children: [
-         Image.asset(
-           kLogo,
-           height: 50,
-         ),
-         Text('chat'),
+    var email = ModalRoute.of(context)!.settings.arguments;
+    sendMessage()
+    {
+      messages.add(
+        {
+          kMessage: controller.text,
+          kCreatedAt: DateTime.now(),
+          'id': email
+        },
+      );
+      controller.clear();
+      _controller.animateTo(0,
+          duration: Duration(milliseconds: 500),
+          curve: Curves.easeIn);
+    }
+    return StreamBuilder<QuerySnapshot>(
+      stream: messages.orderBy(kCreatedAt, descending: true).snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          List<Message> messagesList = [];
+          for (int i = 0; i < snapshot.data!.docs.length; i++) {
+            messagesList.add(Message.fromJson(snapshot.data!.docs[i] ?? ""));
+          }
 
-       ],
-     ),
-     centerTitle: true,
-   ),
-   body: Column(
-     children: [
-     Align(
-     alignment: Alignment.centerLeft,
-     child: Container(
-       padding: EdgeInsets.only(left: 16, top: 32, bottom: 32, right: 32),
-       margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-       decoration: BoxDecoration(
-         borderRadius: BorderRadius.only(
-           topLeft: Radius.circular(32),
-           topRight: Radius.circular(32),
-           bottomRight: Radius.circular(32),
-         ),
-         color: kPrimaryColor,
-       ),
-       child: Text(
-         "message.message",
-         style: TextStyle(
-           color: Colors.white,
-         ),
-       ),
-     ),
-   ),
-       Align(
-         alignment: Alignment.centerRight,
-         child: Container(
-           padding: EdgeInsets.only(left: 16, top: 32, bottom: 32, right: 32),
-           margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-           decoration: BoxDecoration(
-             borderRadius: BorderRadius.only(
-               topLeft: Radius.circular(32),
-               topRight: Radius.circular(32),
-               bottomLeft: Radius.circular(32),
-             ),
-             color: Color(0xff006D84),
-           ),
-           child: Text(
-             "message.message",
-             style: TextStyle(
-               color: Colors.white,
-             ),
-           ),
-         ),
-       ),
-       ElevatedButton(onPressed: (){
-
-         messages.add({"message":"osama"});
-       }, child: Text("test send "))
-     ],
-   ),
- );
+          return Scaffold(
+            appBar: AppBar(
+              automaticallyImplyLeading: false,
+              backgroundColor: kPrimaryColor,
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    kLogo,
+                    height: 50,
+                  ),
+                  Text('chat'),
+                ],
+              ),
+              centerTitle: true,
+            ),
+            body: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                      reverse: true,
+                      controller: _controller,
+                      itemCount: messagesList.length,
+                      itemBuilder: (context, index) {
+                        return messagesList[index].id == email
+                            ? ChatBuble(
+                                message: messagesList[index],
+                              )
+                            : ChatBubleForFriend(message: messagesList[index]);
+                      }),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: TextField(
+                    controller: controller,
+                    onSubmitted: (data) {
+                      sendMessage();
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Send Message',
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          Icons.send,
+                          color: kPrimaryColor,
+                        ),
+                        onPressed: () {
+                          sendMessage();
+                        },
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        borderSide: BorderSide(
+                          color: kPrimaryColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        } else {
+          return Text('Loading...');
+        }
+      },
+    );
   }
 }
